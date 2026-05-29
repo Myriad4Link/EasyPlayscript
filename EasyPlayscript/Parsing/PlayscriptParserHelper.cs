53564ddf2e40e41bdf5fa8e6045a67fbdef79e11
Myriad4Link <2661734285@qq.com>
2026-05-29 23:41:14 +0800

    refactor(parsing): extract PlayscriptError struct from tuple

--- EasyPlayscript/Parsing/PlayscriptParserHelper.cs
+++ EasyPlayscript/Parsing/PlayscriptParserHelper.cs
@@ -5,18 +5,37 @@ using Antlr4.Runtime;
 namespace EasyPlayscript.Parsing;
 
 /// <summary>
+/// Represents a syntax or lexer error in playscript.
+/// </summary>
+public readonly struct PlayscriptError(int line, int col, string msg, bool isLexer)
+{
+    public int Line { get; } = line;
+    public int Col { get; } = col;
+    public string Msg { get; } = msg;
+    public bool IsLexer { get; } = isLexer;
+
+    public void Deconstruct(out int line, out int col, out string msg, out bool isLexer)
+    {
+        line = Line;
+        col = Col;
+        msg = Msg;
+        isLexer = IsLexer;
+    }
+}
+
+/// <summary>
 /// Creates ANTLR lexer/parser instances from playscript source text and collects syntax errors.
 /// </summary>
 public static class PlayscriptParserHelper
 {
-    public static (PlayscriptParser parser, List<(int line, int col, string msg, bool isLexer)> errors) Parse(string input)
+    public static (PlayscriptParser parser, List<PlayscriptError> errors) Parse(string input)
     {
         var inputStream = new AntlrInputStream(input);
         var lexer = new PlayscriptLexer(inputStream);
         var tokens = new CommonTokenStream(lexer);
         var parser = new PlayscriptParser(tokens);
 
-        var errors = new List<(int line, int col, string msg, bool isLexer)>();
+        var errors = new List<PlayscriptError>();
         lexer.RemoveErrorListeners();
         parser.RemoveErrorListeners();
         lexer.AddErrorListener(new CollectingErrorListener(errors, isLexer: true));
@@ -25,27 +44,19 @@ public static class PlayscriptParserHelper
         return (parser, errors);
     }
 
-    private class CollectingErrorListener : IAntlrErrorListener<int>, IAntlrErrorListener<IToken>
+    private class CollectingErrorListener(List<PlayscriptError> errors, bool isLexer)
+        : IAntlrErrorListener<int>, IAntlrErrorListener<IToken>
     {
-        private readonly List<(int line, int col, string msg, bool isLexer)> _errors;
-        private readonly bool _isLexer;
-
-        public CollectingErrorListener(List<(int line, int col, string msg, bool isLexer)> errors, bool isLexer)
-        {
-            _errors = errors;
-            _isLexer = isLexer;
-        }
-
         public void SyntaxError(TextWriter output, IRecognizer recognizer, int offendingSymbol,
             int line, int charPositionInLine, string msg, RecognitionException e)
         {
-            _errors.Add((line, charPositionInLine, msg, _isLexer));
+            errors.Add(new PlayscriptError(line, charPositionInLine, msg, isLexer));
         }
 
         public void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol,
             int line, int charPositionInLine, string msg, RecognitionException e)
         {
-            _errors.Add((line, charPositionInLine, msg, _isLexer));
+            errors.Add(new PlayscriptError(line, charPositionInLine, msg, isLexer));
         }
     }
 }
